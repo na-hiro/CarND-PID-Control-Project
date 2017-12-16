7e3e65649fa359eb5e676559bdb30c85e4f1cf45
na-hiro <hinagai.5963@gmail.com>
2017-12-16 09:31:50 +0900

    Initial commit

---
+++ src/PID.cpp
@@ -0,0 +1,31 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+  this->p_error = 0.0;
+  this->i_error = 0.0;
+  this->d_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+  this->d_error = cte - this->p_error;
+	this->i_error += cte;
+	this->p_error = cte;
+}
+
+double PID::TotalError() {
+  return this->Kp * this->p_error + this->Ki * this->i_error + this->Kd * this->d_error;
+}
